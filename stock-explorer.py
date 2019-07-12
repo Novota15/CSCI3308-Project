6fe83858123fd1fa139b6612fe42e4dd09e87660
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-07-12 14:28:58 -0600

    api working

--- stock-explorer.py
+++ stock-explorer.py
@@ -9,15 +9,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def plot_input():
-    return render_template("market-dash.html") # this should be the name of your html file
+    return render_template("market-dash.html") # name of your html file, pass in variables
 
+# can do get and post
+# set a variable
+# if request.method == 'POST' do post stuff
 @app.route('/', methods=['POST'])
 def plot_input_post():
     text = request.form['ticker']
     stock = Stocker(ticker=text)
     stock.plot_stock()
     filename = text + ".png"
-    return send_file(filename, mimetype='image/png')
+    # return send_file(filename, mimetype='image/png')
+    return render_template("market-dash.html", filename=filename)
 
 
 
