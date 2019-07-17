1d21054fb6cac7bb1a729d09c1c543e1b3d13b46
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-07-17 16:35:55 -0600

    dashboard working

--- stock-explorer.py
+++ stock-explorer.py
@@ -35,7 +35,7 @@ def plot_input_post():
         prediction_plot = text + "-prophet-model.png"
         # return send_file(filename, mimetype='image/png')
         return render_template("Dashboard.html", stock_plot=stock_plot, prediction_plot=prediction_plot)
-    return render_template("Dashboard.html", filename="MSFT.png") # name of your html file, pass in variables
+    return render_template("Dashboard.html", stock_plot="MSFT.png", prediction_plot="MSFT.png") # name of your html file, pass in variables
 
 
 
