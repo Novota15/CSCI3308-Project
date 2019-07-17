a5a2e7fc1a0d6a6ee24654a583fa53e8d491e94a
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-07-17 16:18:42 -0600

    Update stock-explorer.py

--- stock-explorer.py
+++ stock-explorer.py
@@ -14,15 +14,27 @@ app = Flask(__name__)
 # can do get and post
 # set a variable
 # if request.method == 'POST' do post stuff
+
+def plot_history(stock):
+    stock.plot_stock()
+    return
+
+def plot_prediction(stock):
+    model, model_data = stock.create_prophet_model()
+    return model, model_data
+
 @app.route('/', methods=['GET','POST'])
 def plot_input_post():
     if request.method == 'POST':
         text = request.form['ticker']
         stock = Stocker(ticker=text)
-        stock.plot_stock()
-        filename = text + ".png"
+        # stock.plot_stock()
+        plot_history(stock)
+        model, model_data = plot_prediction(stock)
+        stock_plot = text + ".png"
+        prediction_plot = text + "-prophet-model.png"
         # return send_file(filename, mimetype='image/png')
-        return render_template("market-dash.html", filename=filename)
+        return render_template("market-dash.html", stock_plot=stock_plot, prediction_plot=prediction_plot)
     return render_template("market-dash.html", filename="MSFT.png") # name of your html file, pass in variables
 
 
