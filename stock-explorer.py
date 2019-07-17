5c121ff3b131837b850a0abed44755fac38094b0
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-07-17 15:27:34 -0600

    api working

--- stock-explorer.py
+++ stock-explorer.py
@@ -7,21 +7,23 @@ from MarketData.stocker import Stocker
 
 app = Flask(__name__)
 
-@app.route('/')
-def plot_input():
-    return render_template("market-dash.html") # name of your html file, pass in variables
+# @app.route('/')
+# def plot_input():
+#     return render_template("market-dash.html") # name of your html file, pass in variables
 
 # can do get and post
 # set a variable
 # if request.method == 'POST' do post stuff
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def plot_input_post():
-    text = request.form['ticker']
-    stock = Stocker(ticker=text)
-    stock.plot_stock()
-    filename = text + ".png"
-    # return send_file(filename, mimetype='image/png')
-    return render_template("market-dash.html", filename=filename)
+    if request.method == 'POST':
+        text = request.form['ticker']
+        stock = Stocker(ticker=text)
+        stock.plot_stock()
+        filename = text + ".png"
+        # return send_file(filename, mimetype='image/png')
+        return render_template("market-dash.html", filename=filename)
+    return render_template("market-dash.html", filename="MSFT.png") # name of your html file, pass in variables
 
 
 
